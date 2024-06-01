6c6859c869d5b2fa362f7fcaf766cad93aad2fe2
vasu081992 <vasudevan29.92@gmail.com>
2024-06-01 17:51:05 +0530

    code push

---
+++ xlogin/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/LoginPage/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+<Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ xlogin/src/components/LoginPage/Login.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Login() {
+
+    const [username,setusername] = useState('')
+    const [password,setpassword] = useState('')
+
+    const [flag,setflag]=useState(false)
+
+    const[errormessage,setErrorMessage] = useState(false)
+
+
+    const handleSubmit = (e)=>{
+  e.preventDefault();
+
+  if(!username || !password){
+    return
+  }
+
+  if(username==="username" && password==="password"){
+setflag(true)
+  }
+ setErrorMessage(true)
+    }
+
+   
+    
+    const handleUsername = (e)=>{
+        setusername(e.target.value)
+      }
+      
+      const handlePassword = (e)=>{
+        setpassword(e.target.value)
+      }
+
+  return (
+
+
+    <div>
+        
+        <h1>Login Page</h1>
+
+        {
+
+         errormessage && (
+            <h6>Invalid username or password</h6>
+         )
+        }
+    { !flag && (
+        <form onSubmit={handleSubmit}>
+      
+      <div> 
+        
+        <label>Username:</label>
+       <input  type="text" placeholder="username" value={username}onChange={handleUsername} required></input>
+       </div>
+       <label>Password:</label>
+       <input  type="password" placeholder="password" value={password} onChange={handlePassword} required></input>
+       <div> 
+         <button type="submit">Submit</button>
+        </div>
+    
+        </form>
+    )
+}
+{flag && (
+ 
+ <h6>Welcome,user!</h6>
+
+)}
+        </div>
+)
+}
+
+export default Login
